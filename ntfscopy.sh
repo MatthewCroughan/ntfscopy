65e9b490afcfb6ad232c75ee10b120b2bab94364
matthewcroughan <matthewcroughan@protonmail.com>
2019-07-23 21:15:28 +0100

    Add verbose monitoring function for ntfscopy.sh

--- ntfscopy.sh
+++ ntfscopy.sh
@@ -8,6 +8,7 @@ display_usage() {
   echo "    ntfscopy -d /home/pi/files"  
   echo "Commands:"  
   echo "    monitor                               Tail the output of the ntfscopy log"
+  echo "    monitor verbose                       A more verbose version of monitor"
   echo "Options:"  
   echo "    -d                                    Specify the directory containing files with which to copy over with rsync"
   exit 1
@@ -19,9 +20,28 @@ monitor() {
   exit 1
 }
 
+monitor_verbose() {
+  tail -f /var/log/ntfscopy-verbose
+  exit 1
+}
+
+# If more than 2 arguments are specified, then prints out too many arguments
+if (( $# > 2 )); then echo "too many arguments"; exit 1; fi
+
+if [[ $# -eq 0 ]] ; then
+    display_usage
+    echo 'No parameter supplied'
+    exit 0
+fi
 
 # Runs monitor() if monitor is present.
-if [ "$1" == "monitor" ]; then
+if [[ "$1" == "monitor" && "$2" == "verbose" ]]; then
+  monitor_verbose
+  exit 0
+fi
+
+# Runs monitor() if monitor is present.
+if [[ "$1" == "monitor" ]]; then
   monitor
   exit 0
 fi
@@ -48,15 +68,6 @@ if [[ $USER != "root" ]]; then
   exit 1
 fi
 
-# If more than 2 arguments are specified, then prints out 'too many arguments'
-if (( $# > 2 )); then echo "too many arguments"; exit 1; fi
-
-if [[ $# -eq 0 ]] ; then
-    display_usage
-    echo 'No parameter supplied'
-    exit 0
-fi
-
 DISK=/dev/$1
 
 if [ -b "$DISK" ]
@@ -68,31 +79,30 @@ echo $dt Processing drive $1 >> /var/log/ntfscopy
 
 # Debian 10 automounts usbs, this remedies that, and is good practice anyway.
 echo $dt Attempting to unmount all partitions on $1 >> /var/log/ntfscopy
-	umount /dev/"$1"?*
+	umount /dev/"$1"?* >> /var/log/ntfscopy-verbose
 
 # Place green LED on Pi3 in control mode
 	echo gpio | sudo tee /sys/class/leds/led0/trigger
 
 # Turn on the LED heatbeat mode to show activity 
-	echo heartbeat | sudo tee /sys/class/leds/led0/trigger 
+	echo heartbeat | sudo tee /sys/class/leds/led0/trigger
 
 echo $dt Zapping $1 with dd and sfdisk>> /var/log/ntfscopy
 	dd if=/dev/zero of=$DISK bs=4096 count=1 conv=notrunc,fsync
-	echo ',,L' | sudo sfdisk --wipe-partitions=always --wipe=always $DISK --no-reread --force
+	echo ',,L' | sudo sfdisk --wipe-partitions=always --wipe=always $DISK --no-reread --force >> /var/log/ntfscopy-verbose
 
 # Using using sfdisk to create a new DOS partition on the drive
 echo $dt Creating DOS partition on $1 >> /var/log/ntfscopy
-	echo 'type=83' | sudo sfdisk $DISK
+	echo 'type=83' | sudo sfdisk $DISK >>/var/log/ntfscopy-verbose
 
 echo $dt Formatting drive $1 as NTFS  >> /var/log/ntfscopy
 # Formatting the drive as NTFS, requires ntfs-3g
-# sudo apt-get install ntfs-3g
-	sudo mkfs.ntfs -f /dev/"$1"1
+	sudo mkfs.ntfs -f /dev/"$1"1 >> /var/log/ntfscopy-verbose
 
 echo $dt Detaching $1 >> /var/log/ntfscopy
-	sudo udisksctl power-off -b $DISK
+	sudo udisksctl power-off -b $DISK >> /var/log/ntfscopy-verbose
 
-# Turn off LED always
+# Turn off LED 
 	echo 0 | sudo tee /sys/class/leds/led0/brightness 
 
 # Play a sound upon completion
