f54afe134d9024767736e0b3a291ea515a0e1f7c
matthewcroughan <matthewcroughan@protonmail.com>
2019-07-25 17:23:21 +0100

    Fix critical error, did not correctly script the function and provide '' to pass through the argument

--- detected.sh
+++ detected.sh
@@ -4,9 +4,9 @@ SHELL=/bin/bash
 dt=$(date +'%b %d %T:')
 
 echo >> /var/log/ntfscopy
-echo $dt Started ntfscopy.sh >> /var/log/ntfscopy
+echo "$dt" Started ntfscopy.sh >> /var/log/ntfscopy
 start() {
-    echo /usr/local/bin/ntfscopy.sh $1 | at now
+    echo /usr/local/bin/ntfscopy.sh "$1" | at now
 } >> /var/log/ntfscopy-verbose
 
-start
+start "$@"
